4ed5485dd97258e2c06135b141c2ae219d40e6e1
OmerT <omer_tayyab@hotmail.com>
2024-11-20 12:21:12 +0100

    added file to create results dataset for GN dependency over time

---
+++ REALIO/R/dep_all.R
@@ -0,0 +1,14 @@
+#' @import tidyverse
+#' @param years is vector of years
+#' @param cntrys vector of 3-letter country codes as used in relevant dataset
+
+data_dir <- "C:/R_OT/Projects/IO_to_DLS/Data/ICIO"
+
+years <- seq(1995, 2020, 5)
+
+countries <- read_csv(paste0(data_dir, "/ICIO_BEC_conc.csv")) %>%
+  select(c(Code, GS_GN)) %>%
+  filter(GS_GN == "GS")
+
+dep_all <- get_GN_dep_ratio_mult(years, countries$Code) %>%
+  filter(!is.nan(GN_dep_ratio))
