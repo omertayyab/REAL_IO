8310c843063101b818b9f716629921ba19035026
OmerT <omer_tayyab@hotmail.com>
2024-11-25 14:01:04 +0100

    changed dep ratio formula: base changed from dom C to tot imports

--- REALIO/R/get_GN_dep_ratio.R
+++ REALIO/R/get_GN_dep_ratio.R
@@ -4,9 +4,14 @@
 get_GN_dep_ratio <- function(t2) {
 
   r1 <- t2 %>%
-    select(dom_final, imp_GN_final, emb_imp_GN_final) %>%
-    summarize(across(1:3, sum)) %>%
-    mutate(GN_rat_dom = (imp_GN_final + emb_imp_GN_final) / dom_final, .keep = "none") %>%
+    select(imp_GS_final, emb_imp_GS_final, imp_GN_final, emb_imp_GN_final) %>%
+    summarize(across(everything(), sum)) %>%
+    mutate(GN_rat_dom =
+             (imp_GN_final + emb_imp_GN_final) / (imp_GS_final +
+                                                    emb_imp_GS_final +
+                                                    imp_GN_final +
+                                                    emb_imp_GN_final) ,
+           .keep = "none") %>%
     unlist
 
   return(r1)
