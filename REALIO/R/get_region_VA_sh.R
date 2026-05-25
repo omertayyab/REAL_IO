17baf7db1513e77fc67b23497853c6db216b2a71
OmerT <omer_tayyab@hotmail.com>
2026-05-25 16:55:26 +0200

    updated VA share function to calculate by sector

--- REALIO/R/get_region_VA_sh.R
+++ REALIO/R/get_region_VA_sh.R
@@ -10,23 +10,60 @@ get_region_VA_sh <- function(data_dir, years, region_1, sector = NULL)
 
     VA_region <- datalist$icio %>%
       select(contains(region_1)) %>%
-      slice((n()-1)) %>%
-      sum()
+      select(!contains(c("HFCE", "NPISH","GGFC", "GFCF", "INVNT", "DPABR")))
+
+    n_reg_1 <- length(region_1)
+
+    VA_region <-
+      ifelse(!is.null(sector),
+             select(VA_region,which(rep(datalist$conc$DLS == sector, n_reg_1))) %>%
+               slice(n()-1) %>%
+               sum(),
+             slice(VA_region, n()-1) %>%
+               sum()
+           )
 
     VA_total <- datalist$icio %>%
       select(-V1) %>%
-      slice((n()-1)) %>%
-      sum()
+      select(!contains(c("HFCE", "NPISH","GGFC", "GFCF", "INVNT", "DPABR", "OUT")))
+
+    n_ind <- length(datalist$conc$DLS) #no. of industries
+    n_ctry <- length(datalist$cntry_list$Code) #no. of countries
+
+    VA_total <-
+      ifelse(!is.null(sector),
+             select(VA_total,which(rep(datalist$conc$DLS == sector,n_ctry))) %>%
+               slice(n()-1) %>%
+               sum(),
+             slice(VA_total, n()-1) %>%
+               sum()
+      )
 
     X_region <- datalist$icio %>%
-      select(contains(region_1)) %>%
-      slice(n()) %>%
-      sum()
+      select(contains(region_1))
+
+    X_region <-
+      ifelse(!is.null(sector),
+             select(X_region, !contains(c("HFCE", "NPISH","GGFC", "GFCF", "INVNT", "DPABR"))) %>%
+               select(which(rep(datalist$conc$DLS == sector, n_reg_1))) %>%
+               slice(n()) %>%
+               sum(),
+             slice(X_region, n()) %>%
+               sum()
+      )
 
     X_total <- datalist$icio %>%
-      select(-V1) %>%
-      slice(n()) %>%
-      sum()
+      select(-V1, -OUT) %>%
+      select(!contains(c("HFCE", "NPISH","GGFC", "GFCF", "INVNT", "DPABR")))
+
+    X_total <-
+      ifelse(!is.null(sector),
+             select(X_total, which(rep(datalist$conc$DLS == sector,n_ctry ))) %>%
+               slice(n()) %>%
+               sum(),
+             slice(X_total, n()) %>%
+               sum()
+      )
 
     VA_X_tib <- tibble(VA_region = VA_region,
                        X_region = X_region,
@@ -37,13 +74,7 @@ get_region_VA_sh <- function(data_dir, years, region_1, sector = NULL)
              X_sh = X_region/X_all) %>%
       mutate(VA_X_diff = VA_sh - X_sh)
 
-    ##### If sector is specified
-
-    if(!is.null(sector))
-    {
-    }
-
-    else  return(VA_X_tib )
+    return(VA_X_tib )
 
   }
 
